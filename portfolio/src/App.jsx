78850608e6ea250c74156f1a31bae54011ff8849
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-12-13 13:07:54 +0200

    main structure plan

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './UI/App.scss';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="app_container">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,19 @@
+function Nav (){
+    return (
+        <div className='nav_container'>
+            <div className='nav_container_logo'>
+                <div>S</div>
+            </div>
+            <div className='nav_container_links'>
+                <div className='nav_container_links_link'>
+                    <a href='#'>Home</a>
+                    <a href='#'>About</a>
+                    <a href='#'>Contact</a>
+                    <a href='#'>Resume</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
