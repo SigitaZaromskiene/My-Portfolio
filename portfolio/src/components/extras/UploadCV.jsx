cddfdcbf26436b60af015780ea0cadc2507e46fa
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-12-22 11:21:06 +0200

    uploadCV component refactoring

--- portfolio/src/components/extras/UploadCV.jsx
+++ portfolio/src/components/extras/UploadCV.jsx
@@ -2,19 +2,12 @@ import Btn from "../Btn";
 
 function UploadCV() {
   const handleDownload = () => {
-    // Replace 'https://www.yourwebsite.com/path/to/cv.pdf' with the actual direct download URL to your CV file
-    const cvUrl = 'https://www.dropbox.com/scl/fi/ijwmsg1qvjtl8p7hjvry1/CV-FRONT-END.pdf?rlkey=rhm7252orlpmt0kd93x9koril&dl=0'; // Update with your Dropbox link
-    
-    // Create a temporary anchor element
+    const cvUrl = 'https://www.dropbox.com/scl/fi/ijwmsg1qvjtl8p7hjvry1/CV-FRONT-END.pdf?rlkey=rhm7252orlpmt0kd93x9koril&dl=0'; 
     const link = document.createElement('a');
     link.href = cvUrl;
-    link.download = 'cv.pdf'; // The name to be used when downloading
+    link.download = 'cv.pdf'; 
     document.body.appendChild(link);
-
-    // Programmatically trigger the click event to initiate download
     link.click();
-
-    // Clean up - remove the temporary link from the DOM
     document.body.removeChild(link);
   };
 
