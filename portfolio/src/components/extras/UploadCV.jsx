165d2713d565f2487f7e2c98c47ad6cae208d574
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-12-27 17:55:39 +0200

    cv download logic added

--- portfolio/src/components/extras/UploadCV.jsx
+++ portfolio/src/components/extras/UploadCV.jsx
@@ -1,20 +1,12 @@
-import Btn from "../Btn";
+import MyCV from "../../UI/Img/MyCV.pdf";
 
 function UploadCV() {
-  const handleDownload = () => {
-    const cvUrl = 'https://www.dropbox.com/scl/fi/2x3cqykncn0dop0w6ddh2/FRONT-END-CV.pdf?rlkey=i0rem2qeyi1raj1zbgd8ld2qg&dl=0'; 
-    const link = document.createElement('a');
-    link.href = cvUrl;
-    link.target = '_blank'
-    link.download = 'cv.pdf'; 
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <div>
-      <Btn text="RESUME" className="btn + btn_active" action={handleDownload}/>
+      <a className="btn + btn_active" href={MyCV} download="MY_CV.pdf">
+        {" "}
+        RESUME{" "}
+      </a>
     </div>
   );
 }
