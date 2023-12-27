287d2fc64f92287b5fac38bdc51ddf942db1d67b
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-12-27 18:54:25 +0200

    bug fix on nav

--- portfolio/src/components/extras/UploadCV.jsx
+++ portfolio/src/components/extras/UploadCV.jsx
@@ -1,11 +1,12 @@
 import MyCV from "../../UI/Img/MyCV.pdf";
+import Btn from "../Btn";
 
 function UploadCV() {
   return (
-    <a className="btn btn_active" href={MyCV} download="MY_CV.pdf">
+    <Btn className="btn btn_active" href={MyCV} download="MY_CV.pdf">
       {" "}
       RESUME{" "}
-    </a>
+    </Btn>
   );
 }
 
